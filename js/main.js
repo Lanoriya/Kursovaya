ba63f198e124549c07c5cfee71a5861c44713dc7
Lanoriya <Mr_Buzik@mail.ru>
2022-11-03 11:03:30 +0500

    fix

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-$(document).ready(function () {
-    $(".sliders").slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-
-        prevArrow: '<button type="button" class="btn slider-btn slider-prev"><img src="img/left.svg" alt="arrow-left"></button>',
-        nextArrow: '<button type="button" class="btn slider-btn slider-next"><img src="img/right.svg" alt="arrow-right"></button>',
-    })
-});
-
 const openPopUp = document.getElementById('open_pop_up');
 const closePopUp = document.getElementById('pop_up_close');
 const popUp = document.getElementById('pop_up');
