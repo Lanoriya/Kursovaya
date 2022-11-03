41eb470c08868aa14f2017d59cffffcc5c1272c9
Lanoriya <Mr_Buzik@mail.ru>
2022-11-03 10:42:03 +0500

    Удалил карусель

--- js/main.js
+++ js/main.js
@@ -8,3 +8,23 @@ $(document).ready(function () {
         nextArrow: '<button type="button" class="btn slider-btn slider-next"><img src="img/right.svg" alt="arrow-right"></button>',
     })
 });
+
+const openPopUp = document.getElementById('open_pop_up');
+const closePopUp = document.getElementById('pop_up_close');
+const popUp = document.getElementById('pop_up');
+
+openPopUp.addEventListener('click', function(e) {
+    e.preventDefault();
+    popUp.classList.add('active');
+})
+
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('active');
+})
+
+let navButton = document.getElementById('nav-btn');
+let navList = document.getElementById('nav-list');
+
+navButton.onclick = function() {
+    navList.classList.toggle('close');
+}
